433053980044f88d5ef2ed6bf2f8bea1fa5f7647
Tuna <tunaonair@gmail.com>
2023-07-11 11:42:33 +0300

    some changes

--- main.py
+++ main.py
@@ -26,14 +26,11 @@ async def reply(audio_file, message: discord.message.Message):
 async def on_message(message: discord.message.Message):
     if str(message.attachments) == "[]":
         return
-    
     if messageTrigger and (message.content != messageTrigger):
         return
-    
     role_exists = any(role.lower() in [role.name.lower() for role in message.author.roles] for role in requiredRoles)
     if requiredRoles and not role_exists:
         return
-    
     split_v1 = str(message.attachments).split("filename='")[1]
     filename = str(split_v1).split("' ")[0]
     if filename.endswith(allowedExtensions):
