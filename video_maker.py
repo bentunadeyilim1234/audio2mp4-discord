b044e3b7c15f668425b1671be8e43a6657bc48cd
Tuna <tunaonair@gmail.com>
2023-07-11 12:03:53 +0300

    added more settings

--- video_maker.py
+++ video_maker.py
@@ -1,6 +1,10 @@
 from moviepy.editor import *
-import os
-import random
+import os, json
+
+with open("settings.json", "r", encoding='utf-8', errors='ignore') as f:
+    settings = json.load(f)["videoSettings"]
+
+bgColor, textColor, fontName, fontSize, textSpacing = tuple(settings["backgroundColor"]), settings["foreColor"], settings["font"], settings["fontSize"], tuple(settings["spacing"])
 
 def generate(audio_file, text_override=None):
     audio = AudioFileClip(audio_file)
@@ -8,10 +12,10 @@ def generate(audio_file, text_override=None):
 
     path_origin = os.path.basename(audio_file).split('/')[-1]
     textContent = text_override or path_origin
-    text_clip = TextClip(textContent, font="Arial-Medium", color="white", fontsize=30)
-    screenSize = (text_clip.w + 30, text_clip.h + 5)
+    text_clip = TextClip(textContent, font=fontName, color=textColor, fontsize=fontSize)
+    screenSize = tuple(map(sum,zip((text_clip.w, text_clip.h),textSpacing))) #tuple(lambda i, j: i + j, tuple((text_clip.w, text_clip.h)), textSpacing) #(text_clip.w + 30, text_clip.h + 5)
 
-    video = ColorClip(screenSize, color=(0, 0, 0)).set_duration(duration)
+    video = ColorClip(screenSize, color=bgColor).set_duration(duration)
 
     text_clip = text_clip.set_position("center").set_duration(duration)
     video = CompositeVideoClip([video, text_clip])
